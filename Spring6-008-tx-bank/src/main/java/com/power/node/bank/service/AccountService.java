2d8567be3f4935d477b7d43f7d6d72a8236f1f49
darren88881 <darren88881@outlook.com>
2022-11-22 15:21:26 +0800

    spring6-day09-Spring 事务

---
+++ Spring6-008-tx-bank/src/main/java/com/power/node/bank/service/AccountService.java
@@ -0,0 +1,17 @@
+package com.power.node.bank.service;
+
+/**
+ *
+ * @Author darren
+ * @Date 2022/11/16 10:02
+ */
+public interface AccountService {
+
+    /**
+     *
+     * @param fromActno 转出账户
+     * @param toActno 转入账户
+     * @param money 转出金额
+     */
+    void transfer(String fromActno,String toActno,double money);
+}
---
+++ Spring6-008-tx-bank/src/main/java/com/power/node/bank/service/impl/AccountServiceImpl.java
@@ -0,0 +1,49 @@
+package com.power.node.bank.service.impl;
+
+import com.power.node.bank.dao.AccountDao;
+import com.power.node.bank.pojo.Account;
+import com.power.node.bank.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ *
+ * @Author darren
+ * @Date 2022/11/16 10:03
+ */
+@Service("accountService")
+public class AccountServiceImpl implements AccountService {
+
+    private static final int NUM = 2;
+
+    @Autowired
+    private AccountDao accountDao;
+
+    /**
+     * 控制事务
+     * @param fromActno 转出账户
+     * @param toActno 转入账户
+     * @param money 转出金额
+     */
+    @Override
+    @Transactional(rollbackFor = Exception.class)
+    public void transfer(final String fromActno, final String toActno, final double money) {
+        Account fromAccount = accountDao.selectByActno(fromActno);
+
+        if (fromAccount.getBalance() < money){
+            throw new RuntimeException("余额不足");
+        }
+        Account toAccount = accountDao.selectByActno(toActno);
+
+        fromAccount.setBalance(fromAccount.getBalance() - money);
+        toAccount.setBalance(toAccount.getBalance() + money);
+
+        int i = accountDao.updateByActno(fromAccount);
+        i += accountDao.updateByActno(toAccount);
+        if (i != NUM) {
+            throw new RuntimeException("转账失败");
+        }
+    }
+
+}
